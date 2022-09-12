29e3087b81d1378117f0cf71b0282de6517f0a84
OTECH <OTECH@DESKTOP-UORCU8K>
2022-09-12 19:27:02 +0530

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DotnetCoreMVCapp.Models;
+
+namespace DotnetCoreMVCapp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ProductDbContext _context;
+
+        public ProductsController(ProductDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+              return _context.Products != null ? 
+                          View(await _context.Products.ToListAsync()) :
+                          Problem("Entity set 'ProductDbContext.Products'  is null.");
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .FirstOrDefaultAsync(m => m.ProductID == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // GET: Products/AddOrEdit
+        public IActionResult AddOrEdit(int id=0)
+        {
+            if (id == 0)
+                return View(new Product());
+            else
+                return View(_context.Products.Find(id));
+
+        }
+
+        // POST: Products/AddOrEdit
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit([Bind("ProductID,ProductName,Price")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (product.ProductID == 0)
+                {
+                    _context.Add(product);
+
+                }
+                else
+                    _context.Update(product);
+
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+       
+       
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+
+            if (_context.Products == null)
+            {
+                return Problem("Entity set 'ProductDbContext.Products'  is null.");
+            }
+            var product = await _context.Products.FindAsync(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(int id)
+        {
+          return (_context.Products?.Any(e => e.ProductID == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DotnetCoreMVCapp.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        
+        [Column(TypeName ="nvarchar(100)")]
+
+        [Required]
+        public string ProductName { get; set; }
+
+        [Required]
+        public int Price { get; set; }
+    }
+}
